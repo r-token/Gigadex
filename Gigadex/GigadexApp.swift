8dc329f2d343bcca743f88a5a822ba6095bf282d
Ryan Token <ryantoken13@gmail.com>
2025-05-07 19:38:01 -0600

    Initial Commit

---
+++ Gigadex/GigadexApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigadexApp.swift
+//  Gigadex
+//
+//  Created by Ryan Token on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GigadexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
